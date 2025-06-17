f22ba34a55bb10ce7e63b08e5b8d4a92346ed86f
AFTABSIDDIQUE <aftabsiddique2526@gmail.com>
2025-06-18 00:00:34 +0530

    AddedTrainingList

---
+++ HRMSTeam3/Admin/AddTrainingList.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="AddTrainingList.aspx.cs" Inherits="HRMSTeam3.Admin.AddTrainingList" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+        &nbsp;Add Training List</p>
+    <p>
+        &nbsp;</p>
+    <p>
+&nbsp; Trainer</p>
+    <p>
+        &nbsp;&nbsp;<asp:DropDownList ID="DropDownList1" runat="server">
+        </asp:DropDownList>
+    </p>
+    <p>
+&nbsp; Training Type</p>
+    <p>
+&nbsp;
+        <asp:DropDownList ID="DropDownList2" runat="server">
+        </asp:DropDownList>
+    </p>
+    <p>
+        &nbsp;Employee</p>
+    <p>
+&nbsp;
+        <asp:DropDownList ID="DropDownList3" runat="server">
+        </asp:DropDownList>
+    </p>
+    <p>
+        &nbsp;Training Cost</p>
+&nbsp;
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <p>
+        &nbsp;Description</p>
+    <p>
+        &nbsp;&nbsp;<textarea id="TextArea1" cols="20" name="S1" rows="2"></textarea></p>
+    <p>
+        &nbsp;Status</p>
+    <p>
+        &nbsp;&nbsp;<asp:DropDownList ID="DropDownList4" runat="server">
+        </asp:DropDownList>
+    </p>
+    <p>
+        &nbsp;Start Date</p>
+&nbsp;
+    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    <br />
+    <p>
+        &nbsp;End Date</p>
+&nbsp;
+    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    <p>
+&nbsp; &nbsp;&nbsp;<asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Add" />
+    </p>
+</asp:Content>
---
+++ HRMSTeam3/Admin/AddTrainingList.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HRMSTeam3.Admin
+{
+    public partial class AddTrainingList : System.Web.UI.Page
+    {
+        SqlConnection conn;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cnf = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+            conn = new SqlConnection(cnf);
+            conn.Open();
+            fetchEmp();
+            fetchTrainer();
+            fetchTrainingType();
+
+        }
+        public void fetchEmp()
+        {
+            string q = "select fname from userCreation where roleId='User'";
+            SqlCommand cmd = new SqlCommand(q, conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+            if (rdr.HasRows)
+            {
+                while (rdr.Read())
+                {
+                    string role = rdr["fname"].ToString();
+                    DropDownList3.Items.Add(role);
+                }
+            }
+
+        }
+        public void fetchTrainer()
+        {
+            string q = "select fname from trainerDetails";
+            SqlCommand cmd = new SqlCommand(q, conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+            if (rdr.HasRows)
+            {
+                while (rdr.Read())
+                {
+                    string role = rdr["fname"].ToString();
+                    DropDownList1.Items.Add(role);
+                }
+            }
+
+        }
+
+        public void fetchTrainingType()
+        {
+            string q = "select trainingname from TrainingType";
+            SqlCommand cmd = new SqlCommand(q, conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+            if (rdr.HasRows)
+            {
+                while (rdr.Read())
+                {
+                    string role = rdr["trainingname"].ToString();
+                    DropDownList2.Items.Add(role);
+                }
+            }
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
